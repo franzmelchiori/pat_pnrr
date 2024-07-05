6b1d8bacdcb109f3fe7491af11d43424059a5882
franzmelchiori <franzmelchiori@gmail.com>
2024-07-05 15:00:01 +0200

    update pat-pnrr mpe report comunale

--- pat_pnrr_mpe/pat_pnrr_mpe_report_comunale.py
+++ pat_pnrr_mpe/pat_pnrr_mpe_report_comunale.py
@@ -4,22 +4,27 @@
 """
 
 
+import os
 import subprocess
 
 from .pat_pnrr_comuni_excel_mapping import *
 
 
-def print_report_comunali():
-    with open('pat_pnrr_mpe\\report_comunale\\pat_pnrr_mpe_report_comunale.tex') as f1:
-        tex_pat_pnrr_mpe_report_comunale = f1.read()
-    f1.close()
-
-    name_comune = 'Ledro'
+def print_report_comunale(name_comune, version):
+    name_comune = name_comune
     name_comune_file = name_comune
     name_comune_file = name_comune_file.replace('à', 'a')
     name_comune_file = name_comune_file.replace('è', 'e')
     name_comune_file = name_comune_file.replace('é', 'e')
     name_comune_file = name_comune_file.replace('ù', 'u')
+    version = version
+    version_file = version
+    version_file = version_file.replace('.', '-')
+
+    with open('pat_pnrr_mpe\\report_comunale\\pat_pnrr_mpe_report_comunale.tex', \
+              mode='r', encoding='utf-8') as f1:
+        tex_pat_pnrr_mpe_report_comunale = f1.read()
+    f1.close()
 
     tex_pat_pnrr_mpe_report_comunale = \
         tex_pat_pnrr_mpe_report_comunale.replace(
@@ -27,22 +32,61 @@ def print_report_comunali():
     tex_pat_pnrr_mpe_report_comunale = \
         tex_pat_pnrr_mpe_report_comunale.replace(
             '<NAME_COMUNE_FILE>', name_comune_file)
-    
-    f2 = open('pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_report_comunale_' +
-        name_comune_file + '.tex', 'a')
+    tex_pat_pnrr_mpe_report_comunale = \
+        tex_pat_pnrr_mpe_report_comunale.replace(
+            '<VERSION>', version)
+
+    try:
+        os.remove('pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_' + \
+                name_comune_file + '_' + version_file + '.tex')
+    except:
+        pass
+    f2 = open('pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_' +
+        name_comune_file + '_' + version_file + '.tex', mode='w', encoding='utf-8')
     f2.write(tex_pat_pnrr_mpe_report_comunale)
     f2.close()
 
     subprocess.call(['pdflatex', '-output-directory',
                      'pat_pnrr_mpe\\report_comunale\\pdfs\\',
-                     'pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_report_comunale_' + \
-                        name_comune_file + '.tex'])
+                     'pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_' + \
+                      name_comune_file + '_' + version_file + '.tex'])
     subprocess.call(['pdflatex', '-output-directory',
                      'pat_pnrr_mpe\\report_comunale\\pdfs\\',
-                     'pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_report_comunale_' + \
-                        name_comune_file + '.tex'])
+                     'pat_pnrr_mpe\\report_comunale\\texes\\pat_pnrr_mpe_' + \
+                      name_comune_file + '_' + version_file + '.tex'])
+    
+    try:
+        os.remove('pat_pnrr_mpe\\report_comunale\\pdfs\\pat_pnrr_mpe_' + \
+                name_comune_file + '_' + version_file + '.aux')
+    except:
+        pass
+    try:                
+        os.remove('pat_pnrr_mpe\\report_comunale\\pdfs\\pat_pnrr_mpe_' + \
+                name_comune_file + '_' + version_file + '.idx')
+    except:
+        pass
+    try:                
+        os.remove('pat_pnrr_mpe\\report_comunale\\pdfs\\pat_pnrr_mpe_' + \
+                name_comune_file + '_' + version_file + '.log')
+    except:
+        pass
+    try:                
+        os.remove('pat_pnrr_mpe\\report_comunale\\pdfs\\pat_pnrr_mpe_' + \
+                name_comune_file + '_' + version_file + '.toc')
+    except:
+        pass
+    return
+
+
+def print_report_comunali(version, just_one = False):
+    name_comuni = [comune[0] for comune in comuni_excel_map]
+    for name_comune in name_comuni:
+        print_report_comunale(name_comune, version = version)
+        if just_one:
+            break
     return
 
 
 if __name__ == '__main__':
-    print_report_comunali()
+    # print_report_comunale(name_comune = 'Baselga di Pinè', version = 'v5.4.0')
+    print_report_comunali(version = 'v5.4.0')
