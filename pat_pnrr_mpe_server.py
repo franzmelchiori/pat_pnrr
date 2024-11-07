1a37ddc642f5e1d0401278b7f5f144acb4979246
franzmelchiori <franzmelchiori@gmail.com>
2024-11-07 22:16:39 +0100

    update pat mpe web app

--- pat_pnrr_mpe_server.py
+++ pat_pnrr_mpe_server.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask
 
 TEMPLATE_FOLDER = 'pat_pnrr_mpe_web_app/pages'
+STATIC_FOLDER = 'pat_pnrr_mpe_web_app/static'
 
-app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
 from pat_pnrr_mpe_web_app import *
 
 
--- pat_pnrr_mpe_web_app/__init__.py
+++ pat_pnrr_mpe_web_app/__init__.py
@@ -1,4 +1,5 @@
-from flask import render_template, request
+from flask import render_template, url_for, request
+from markupsafe import escape, Markup
 
 from pat_pnrr.pat_pnrr_mpe_server import app
 
@@ -11,6 +12,14 @@ def index():
 # def project():
 #     return '<h1>project</h1>'
 
-# @app.route('/mpe_comunale/<name_comune>')
+# @app.route('/mpe_comunale/<name_comune>', methods=['GET', 'POST'])
 # def mpe_comunale(name_comune):
-#     return f'<h1>mpe del comune di {name_comune}<h1>'
+#     arg = request.args.get('arg')
+#     if request.method == 'POST':
+#         pass
+#     return render_template(f'<h1>mpe del comune di {escape(name_comune)}<h1>', name_comune=name_comune, arg=arg)
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('project'))
+#     print(url_for('mpe_comunale', name_comune='trento'))
