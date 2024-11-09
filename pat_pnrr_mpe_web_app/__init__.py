4cb26650cc3aeb7b254fdb7c171978c7d38017ba
franzmelchiori <franzmelchiori@gmail.com>
2024-11-09 22:22:11 +0100

    update pat mpe web app

--- pat_pnrr_mpe_web_app/__init__.py
+++ pat_pnrr_mpe_web_app/__init__.py
@@ -14,7 +14,7 @@ def index():
         'pdc_ov_arretrati': 324,
         'pds_durata': 130,
         'pds_arretrati': 300}
-    btnradio_mpe = 'btnradio_mpe_2024Q1_2'
+    btnradio_mpe = request.args.get('btnradio_mpe')
     if btnradio_mpe == 'btnradio_mpe_2024Q1_2':
         comuni_pdc_ov_measure, comuni_monitored = pat_pnrr_6a_misurazione.get_comuni_measure(
             pat_pnrr_6a_misurazione.comuni_excel_map, 'Permessi di Costruire',
@@ -34,8 +34,9 @@ def index():
             pdc = np.ceil(comuni_pdc_measure.values).astype(int),
             cila = np.ceil(comuni_cila_measure.values).astype(int),
             comuni = comuni_monitored,
-            target = target)
-    if btnradio_mpe == 'btnradio_mpe_2023Q3_4':
+            target = target,
+            btnradio_mpe = btnradio_mpe)
+    elif btnradio_mpe == 'btnradio_mpe_2023Q3_4':
         comuni_pdc_ov_measure, comuni_monitored = pat_pnrr_5a_misurazione.get_comuni_measure(
             pat_pnrr_5a_misurazione.comuni_excel_map, 'Permessi di Costruire',
             'pat_pnrr_5a_misurazione_tabelle_comunali\\')
@@ -54,7 +55,8 @@ def index():
             pdc = np.ceil(comuni_pdc_measure.values).astype(int),
             cila = np.ceil(comuni_cila_measure.values).astype(int),
             comuni = comuni_monitored,
-            target = target)
+            target = target,
+            btnradio_mpe = btnradio_mpe)
 
 # @app.route('/progetto')
 # def project():
