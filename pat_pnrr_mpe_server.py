ebb3bcc10d007070dac80ef4f5a9cd8e52c427e6
franzmelchiori <franzmelchiori@gmail.com>
2025-03-21 14:08:16 +0100

    up pat mpe

--- pat_pnrr_mpe_server.py
+++ pat_pnrr_mpe_server.py
@@ -3,10 +3,13 @@ from flask import Flask
 
 TEMPLATE_FOLDER = 'pat_pnrr_mpe_web_app/pages'
 STATIC_FOLDER = 'pat_pnrr_mpe_web_app/static'
+CERT_FOLDER = 'pat_pnrr_mpe_web_app/cert'
 
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
 from pat_pnrr_mpe_web_app import *
 
 
 if __name__ == '__main__':
-    app.run()
+    context = (CERT_FOLDER + '/' + 'localserver.crt',
+               CERT_FOLDER + '/' + 'localserver.key')
+    app.run(host='127.0.0.1', ssl_context=context)
