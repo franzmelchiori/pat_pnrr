29c553fd504a22fb809a4109c9d358cb1eaf0bfd
franzmelchiori <franzmelchiori@gmail.com>
2025-06-16 22:48:36 +0200

    up pat ssu

--- pat_pnrr_ssu/moduli_pat/04_forms/mod_pat_validator.py
+++ pat_pnrr_ssu/moduli_pat/04_forms/mod_pat_validator.py
@@ -1,10 +1,14 @@
 import xmlschema
 
+XSD_FOR_VALIDATION = 'mod_pat_edilizia_comunicazione_inizio_lavori_v0.1.0.xsd'
+XML_TO_VALIDATE = 'mod_pat_edilizia_comunicazione_inizio_lavori_v0.1.0.xml'
 
-schema_file = open('mod_pat_edilizia_comunicazione_inizio_lavori_v0.1.0.xsd')
+
+schema_file = open(XSD_FOR_VALIDATION)
 schema = xmlschema.XMLSchema(schema_file)
 
-xml_file = open('mod_pat_edilizia_comunicazione_inizio_lavori_v0.1.0.xml')
+xml_file = open(XML_TO_VALIDATE)
 if schema.is_valid(xml_file):
     print('XML valido')
-schema.validate(xml_file)
+else:
+    schema.validate(xml_file)
