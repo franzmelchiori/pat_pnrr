be68b854ce2f3d018bc8a468b567eedf026cf556
franzmelchiori <franzmelchiori@gmail.com>
2025-09-23 18:48:08 +0200

    up pat mpe

--- pat_pnrr_mpe/pat_pnrr_8a_misurazione.py
+++ pat_pnrr_mpe/pat_pnrr_8a_misurazione.py
@@ -2264,9 +2264,11 @@ if __name__ == '__main__':
     # get_comuni_measures_dataframe(comuni_excel_map, load=False, tsf=False)
     # get_comuni_measures(comuni_excel_map, tsf=False)
     
+    # get_comuni_dataframes(comuni_excel_map, load=False)  # 1 df di pratiche per 1 ped per tutti i comuni
+    # get_comuni_measures_dataframe(comuni_excel_map, load=False, tsf=False)  # 1 df di misure per 1 ped per tutti i comuni
     # get_comuni_dataframes(comuni_excel_map, load=False, sf='t_01')  # 1 df di pratiche per 1 ped per tutti i comuni
     # get_comuni_measures_dataframe(comuni_excel_map, load=False, tsf=True)  # 1 df di misure per 1 ped per tutti i comuni
-    get_comuni_measures(comuni_excel_map, tsf=True)  # stampa 8 misure per tutti i ped da tutti i comuni
+    # get_comuni_measures(comuni_excel_map, tsf=True)  # stampa 8 misure per tutti i ped da tutti i comuni
 
 
     # load = True
